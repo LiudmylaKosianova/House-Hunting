2d5ded6e2dad0ec1f33ff6d1399084dc0b463e87
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-01-26 17:41:44 +0100

    ask for input

--- ps1a.py
+++ ps1a.py
@@ -2,7 +2,7 @@
 A program calculates how many months it will take a user to save up enough money 
 for a down payment for a dream house. 
 The programm assumes the user has some investments that return 4% annually. 
- 
+
 """
 
 annual_salary = 0.0
@@ -11,5 +11,14 @@ portion_saved = 0.0 #a portion of salary to be saved every month
 total_cost = 0.0 #cost of a house
 portion_down_payment = 0.25 #the portion of a cost needed for the down payment is 25%
 r = 0.04 #annual return from the current_savings investments
+
+user_input = input("Enter your annual salary: ")
+annual_salary = float(user_input)
+
+user_input = input("Enter the percent of your salary to save: ")
+portion_saved = float(user_input) / 100
+
+user_input = input("Enter the cost of your dream home: ")
+total_cost = float(user_input)
 
 
