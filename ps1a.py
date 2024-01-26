f02979a788c6f11e582fc391130983eb2a918c0a
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2024-01-26 17:09:55 +0100

    set the variables

---
+++ ps1a.py
@@ -0,0 +1,15 @@
+"""
+A program calculates how many months it will take a user to save up enough money 
+for a down payment for a dream house. 
+The programm assumes the user has some investments that return 4% annually. 
+ 
+"""
+
+annual_salary = 0.0
+monthly_salary = annual_salary / 12 
+portion_saved = 0.0 #a portion of salary to be saved every month
+total_cost = 0.0 #cost of a house
+portion_down_payment = 0.25 #the portion of a cost needed for the down payment is 25%
+r = 0.04 #annual return from the current_savings investments
+
+
